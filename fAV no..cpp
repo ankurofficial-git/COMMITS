b0f17f3184dd575ca8db4eb9b7fb8abd95865af8
ankurofficial-git <ankur.official.252007@gmail.com>
2026-09-12 23:37:44 +0530

    Favourite Number

---
+++ fAV no..cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+	int favouritenumber;
+	printf("Enter your favourite number:");
+	scanf("%d",&favouritenumber);
+	printf("Your favourite number is: %d\n", favouritenumber);
+	return 0;
+}
+	
